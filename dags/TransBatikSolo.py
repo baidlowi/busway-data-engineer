3e011f58e62843495eb3b77f2a03f94fe2728650
baidlowi <79616397+baidlowi@users.noreply.github.com>
2024-06-18 22:13:23 +0700

    update check file gcs

---
+++ dags/TransBatikSolo.py
@@ -0,0 +1,205 @@
+import os
+import logging
+import re
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.exceptions import AirflowSkipException
+from airflow.utils.email import send_email
+
+from google.cloud import storage
+from airflow.contrib.sensors.gcs_sensor import GoogleCloudStorageObjectSensor
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+from airflow.contrib.operators.bigquery_operator import BigQueryOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+import pandas
+import pyarrow as pa
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+
+year = 2023
+month = "agustus"
+
+# Define the website URL
+url = f"https://data.surakarta.go.id/dataset/data-penumpang-bst-bulan-{month}-{year}"
+response = requests.get(url)
+if response.status_code == 200:
+  soup = BeautifulSoup(response.content, 'html.parser')
+  data_elements = soup.find_all(attrs={"data-id": True})
+  if data_elements:
+    for element in data_elements:
+      data_id = element.get('data-id')
+
+dataset_file = f"{year}-{month}-Data-Penumpang-Bus-Surakarta.xlsx"
+dataset_url = f"https://data.surakarta.go.id/dataset/data-penumpang-bst-bulan-{month}-{year}/resource/{data_id}/download"
+    
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+parquet_file = dataset_file.replace('.xlsx', '.parquet')
+GCS_PARQUET = f"raw/surakarta/{parquet_file}"
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'busway')
+RAW_DATASET = os.environ.get("RAW_DATASET", 'raw')
+
+def format_to_parquet(src_file):
+    if not src_file.endswith('.xlsx'):
+        logging.error("Can only accept source files in xlsx format, for the moment")
+        return
+    table = pandas.read_excel(src_file, skiprows=1)
+
+    # Define a function to extract year and month from filename
+    def extract_year_month(src_file):
+      pattern = r"(\d{4})-(\w+)" 
+
+      match = re.search(pattern, src_file)
+      if match:
+        year, month = match.groups()
+        return year, month
+      else:
+        return None, None
+
+    year, month = extract_year_month(src_file)
+    table['year'] = year
+    table['month'] = month
+
+    # Save the modified DataFrame to a new file (optional)
+    table = pa.Table.from_pandas(table.astype(str))
+    pq.write_table(table, src_file.replace('.xlsx', '.parquet'))
+
+def check_file_exists(**kwargs):
+    gcs_list_operator = GoogleCloudStorageObjectSensor(
+        task_id="check_file_exists",
+        bucket=BUCKET,
+        object=GCS_PARQUET,
+        poke_interval=10,
+        timeout=60,
+    )
+    results = gcs_list_operator.poke(context=kwargs)
+
+    if results:
+        raise AirflowSkipException
+    else:
+        return True
+
+def upload_to_gcs(bucket, object_name, local_file):
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
+
+default_args = {
+    "owner": "HASYIM",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# Task Flow 
+with DAG(
+    dag_id="etl-surakarta",
+    schedule_interval="@daily",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['etl-srkt'],
+) as dag:
+    # Download Dataset
+    download_dataset = BashOperator(
+        task_id="download_dataset",
+        bash_command=f"curl {dataset_url} >> {path_to_local_home}/{dataset_file}"
+    )
+
+    # Format dataset csv to parquet
+    format_to_parquet = PythonOperator(
+        task_id="format_to_parquet",
+        python_callable=format_to_parquet,
+        op_kwargs={
+            "src_file": f"{path_to_local_home}/{dataset_file}",
+        },
+    )
+    
+    # Check if target Parquet file exists on GCS
+    check_file_task = PythonOperator(
+        task_id="check_file_task",
+        python_callable=check_file_exists,
+        provide_context=True,
+        on_failure_callback=lambda context: context["task_instance"].xcom_push(key="skipped", value=True),
+    )
+
+    # Upload file local to GCS
+    local_to_gcs = PythonOperator(
+        task_id="local_to_gcs",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/surakarta/{parquet_file}",
+            "local_file": f"{path_to_local_home}/{parquet_file}",
+        },
+    )
+
+    # Delete local file dataset
+    delete_local_data = BashOperator(
+        task_id="delete_local_data",
+        bash_command=f"rm -rf {path_to_local_home}/*Data-Penumpang-*",
+    )
+
+    # Create external temp table
+    bigquery_external_table = BigQueryInsertJobOperator(
+        task_id=f"bigquery_external_table",
+        configuration={
+            "query": {
+                "query": 
+                    f"""
+                    CREATE OR REPLACE EXTERNAL TABLE `{PROJECT_ID}.raw.srkt_busway`
+                    OPTIONS (
+                      format ="PARQUET",
+                      uris = ['gs://{BUCKET}/{GCS_PARQUET}']
+                    );
+                    """,
+                "useLegacySql": False,
+            }
+        },
+        dag = dag
+    )
+    
+    # Create table and transform data from external table
+    bq_create_table = BigQueryInsertJobOperator(
+        task_id=f"bq_create_table",
+        configuration={
+            "query": {
+                "query": 
+                    """
+                    CREATE TABLE IF NOT EXISTS `busway.transbatiksolo`
+                    (
+                    `tahun` INT64,
+                    `bulan` STRING,
+                    `rute` STRING,
+                    `jumlah_penumpang` INT64,
+                    PRIMARY KEY (tahun, bulan, rute) NOT ENFORCED,
+                    );
+
+                    INSERT INTO `busway.transbatiksolo` (
+                      `tahun`,
+                      `bulan`,
+                      `rute`,
+                      `jumlah_penumpang`
+                    )
+                    select CAST(year AS INT64) as tahun, Upper(month) AS bulan, Rute as rute, CAST(Total_Penumpang AS INT64) as jumlah_penumpang
+                    from `raw.srkt_busway`;
+                    """,
+                "useLegacySql": False,
+            }
+        },
+        dag = dag
+    )
+
+    # Flow in Airflow
+    download_dataset >> format_to_parquet >> check_file_task  >> local_to_gcs >> bigquery_external_table >> bq_create_table
+    local_to_gcs >> delete_local_data
